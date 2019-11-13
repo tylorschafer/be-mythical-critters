7118d78ebc23ffd120d956e43b45a0276184746a
Tylor Schafer <tylorschafer@gmail.com>
2019-11-12 23:36:32 -0700

    Add ogre

---
+++ exercises/human.js
@@ -0,0 +1,12 @@
+class Human {
+    constructor(name) {
+        this.name = name;
+        this.encounterCounter = 0;
+        this.knockedOut = false;
+    }
+
+    noticesOgre() {
+        return (this.encounterCounter % 3 === 0) && (this.encounterCounter != 0);
+    }
+}
+module.exports = Human;
