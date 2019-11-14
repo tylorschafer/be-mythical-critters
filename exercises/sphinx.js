60a53db01ad2ad6450bf8d447f8e39800256aacc
Tylor Schafer <tylorschafer@gmail.com>
2019-11-13 20:03:07 -0700

    Add sphinx

---
+++ exercises/sphinx.js
@@ -0,0 +1,32 @@
+class Sphinx {
+    constructor() {
+        this.name = null;
+        this.riddles = [];
+        this.heroesEaten = 0;
+    }
+
+    collectRiddle(riddle) {
+        this.riddles.push(riddle);
+        if (this.riddles.length > 3) {
+            this.riddles.shift();
+        }
+    }
+
+    attemptAnswer(attempt) {
+        let result = this.riddles.find(o => o.answer === attempt);
+        let position = this.riddles.indexOf(result);
+        if (result) {
+            this.riddles.splice(position, 1);
+            if (this.riddles.length === 0) {
+                return 'PSSSSSSS THIS HAS NEVER HAPPENED, HOW DID YOU KNOW THE ANSWER WAS ' + '"' + attempt + '"' + '???';
+            }
+            else {
+                return 'That wasn\'t that hard, I bet you don\'t get the next one';
+            }
+        }
+        else {
+            this.heroesEaten += 1;
+        }
+    }
+}
+module.exports = Sphinx;
