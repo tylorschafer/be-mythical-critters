7bd7482a696158549508678b2f838b95d89a89d9
Tylor Schafer <tylorschafer@gmail.com>
2019-11-14 21:51:25 -0700

    Refactor medusa and werewolf styling

--- exercises/medusa.js
+++ exercises/medusa.js
@@ -1,21 +1,20 @@
 class Medusa {
-    constructor(name) {
-        this.name = name;
-        this.statues = [];
-    }
+  constructor (name) {
+    this.name = name
+    this.statues = []
+  }
 
-    stare(victim) {
-        if (this.statues.length < 3) {
-          this.statues.push(victim);
-          victim.stoned = true;
-        }
-        else {
-          var unstoned = this.statues.shift();
-          unstoned.stoned = false;
-          this.statues.push(victim);
-          victim.stoned = true;
-        }
+  stare (victim) {
+    if (this.statues.length < 3) {
+      this.statues.push(victim)
+      victim.stoned = true
+    } else {
+      var unstoned = this.statues.shift()
+      unstoned.stoned = false
+      this.statues.push(victim)
+      victim.stoned = true
     }
+  }
 }
 
-module.exports = Medusa;
+module.exports = Medusa
--- exercises/werewolf.js
+++ exercises/werewolf.js
@@ -1,27 +1,26 @@
 class Werewolf {
-    constructor(name, location, human = true, wolf = false, hungry = false) {
-        this.name = name;
-        this.location = location;
-        this.human = human;
-        this.wolf = wolf;
-        this.hungry = hungry;
-    }
+  constructor (name, location, human = true, wolf = false, hungry = false) {
+    this.name = name
+    this.location = location
+    this.human = human
+    this.wolf = wolf
+    this.hungry = hungry
+  }
 
-    change() {
-        this.human = !this.human;
-        this.wolf = !this.wolf;
-        this.wolf === true ? this.hungry = true : this.hungry = false;
-    }
+  change () {
+    this.human = !this.human
+    this.wolf = !this.wolf
+    this.wolf === true ? this.hungry = true : this.hungry = false
+  }
 
-    eat(victim) {
-        if (this.hungry === false) {
-            return 'I cannot eat because I am not hungry.';
-        }
-        else {
-            victim.alive = false;
-            this.change();
-            return 'YUM!';
-        }
+  eat (victim) {
+    if (this.hungry === false) {
+      return 'I cannot eat because I am not hungry.'
+    } else {
+      victim.alive = false
+      this.change()
+      return 'YUM!'
     }
+  }
 }
-module.exports = Werewolf;
+module.exports = Werewolf
